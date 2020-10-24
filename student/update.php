48eed015a8f97f2cf3602a65750cfb9432016a31
Al-AminJoy <alaminislam3555@gmail.com>
2020-10-24 23:25:04 +0600

    Complete

---
+++ student/update.php
@@ -0,0 +1,36 @@
+<?php
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+include('../class/Student.php');
+$student = new Student();
+switch($requestMethod) {	
+	case 'POST':
+		$data = json_decode(file_get_contents('php://input'), true);
+		$studentID = $data['id'];
+		$first_name = $data['first_name'];
+		$last_name = $data['last_name'];
+		$roll_no = $data['roll_no'];
+		$class = $data['class'];
+		$age = $data['age'];
+		$address = $data['address'];
+		//$status = $data['status'];
+ 
+		$student->setStudentID($studentID);
+	    $student->setFirstName($first_name);
+	    $student->setLastName($last_name);
+	    $student->setRollNo($roll_no);
+	    $student->setClassName($class);
+	    $student->setAge($age);
+	    $student->setAddress($address);
+		$studentInfo = $student->updateStudent();
+		if(!empty($studentInfo)) {
+	      $js_encode = json_encode(array('status'=>TRUE, 'message'=>'Student updated Successfully'), true);
+        } else {
+            $js_encode = json_encode(array('status'=>FALSE, 'message'=>'Student updation failed.'), true);
+        }
+		header('Content-Type: application/json');
+		echo $js_encode;
+	default:
+	header("HTTP/1.0 405 Method Not Allowed");
+	break;
+}
+?>	
